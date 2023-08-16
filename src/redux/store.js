93e1ad7b174d7b40432e7ff173f0e5c6aa5d8fa8
Deepak0056 <94425277+Deepak0056@users.noreply.github.com>
2023-08-16 22:41:25 +0545

    Update

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+// // import { applyMiddleware, compose } from 'redux';
+// import { applyMiddleware, compose } from '@reduxjs/toolkit';
+// import { configureStore } from '@reduxjs/toolkit';
+// import thunk from 'redux-thunk';
+// import {productReducer} from './reducer/fetchApi';
+
+
+// // const reducer = combineReducers({
+// //   productReducer
+// // })
+// export default store = configureStore({reducer: productReducer}, compose(applyMiddleware(thunk), window._REDUX_DEVTOOLS_EXTENSION_&& window._REDUX_DEVTOOLS_EXTENSION_())
+// }
